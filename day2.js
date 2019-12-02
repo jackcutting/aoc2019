e6f0a741d2d9106dcb07a0b9ef261e9e7f8906cb
Jack Cutting <me@jackcutting.co.uk>
2019-12-02 10:03:04 +0000

    Day 2 Part 2

--- day2.js
+++ day2.js
@@ -1,9 +1,6 @@
 const fs = require('fs');
 const input = fs.readFileSync('./input/day2.txt', 'utf8').trim().split(',');
 
-input[1] = '12';
-input[2] = '2';
-
 const add = (a, b) => {
   return a + b;
 };
@@ -12,29 +9,59 @@ const multi = (a, b) => {
   return a * b;
 }
 
-let running = true;
-let index = 0;
+const run = (noun, verb) => {
+  const memory = [...input];
+  memory[1] = noun.toString();
+  memory[2] = verb.toString();
+  let running = true;
+  let index = 0;
 
-while (running) {
-  const op = input[index];
-  const a = parseInt(input[input[index + 1]]);
-  const b = parseInt(input[input[index + 2]]);
-  const loc = input[index + 3];
+  while (running) {
+    const op = memory[index];
+    const a = parseInt(memory[memory[index + 1]]);
+    const b = parseInt(memory[memory[index + 2]]);
+    const loc = memory[index + 3];
 
-  switch (op) {
-    case '1':
-      input[loc] = add(a, b);
+    switch (op) {
+      case '1':
+      memory[loc] = add(a, b);
       break;
-    case '2':
-      input[loc] = multi(a, b);
+      case '2':
+      memory[loc] = multi(a, b);
       break;
-    case '99':
-    default:
+      case '99':
+      default:
       running = false;
       break;
+    }
+
+    index = index + 4;
   }
 
-  index = index + 4;
+  return parseInt(memory[0]);
 }
 
-console.info('Answer: ', input[0]);
+let found = false;
+let noun = 12;
+let verb;
+const max = 99;
+
+while (found == false) {
+  verb = 0;
+  for (let i = 0; i < 99; i++) {
+    verb = i;
+
+    if (run(noun, verb) === 19690720) {
+      found = true;
+      break;
+    }
+  }
+
+  if (!found) {
+    noun++;
+  }
+}
+
+const answer = (100 * noun) + verb;
+
+console.info('Answer: ', answer);
